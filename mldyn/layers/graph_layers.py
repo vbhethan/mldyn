b89548c0b0fe52dfe1b3efbb8e4737a7503afc93
Vignesh Bhethanabotla <vbhethan@caltech.edu>
2023-08-15 21:15:13 -0700

    updates layer code for node2edge and edge2node implementation

---
+++ mldyn/layers/graph_layers.py
@@ -0,0 +1,41 @@
+import torch
+from torch import Tensor
+from torch import nn
+from torch_geometric.nn import MessagePassing
+from torch_geometric.typing import OptPairTensor, Adj, OptTensor, Size
+
+class Node2Edge(torch.nn.Module):
+    def __init__(self, n_node_features, n_edge_features, hidden_edge_features, out_edge_features):
+        super().__init__()
+        self.edge_mlp = nn.Sequential(
+            nn.Linear(2*n_node_features + n_edge_features, hidden_edge_features),
+            nn.ReLU(),
+            nn.Linear(hidden_edge_features, out_edge_features)
+        )
+
+    def forward(self, src, dst, edge_attr):
+        out = torch.cat([src, dst, edge_attr], 1)
+        out = self.edge_mlp(out)
+
+        return out
+    
+
+class Node2Edge_v2(MessagePassing):
+    # This one implements the edge updates with the Pyg MessagePassing class
+    def __init__(self, n_node_features, n_edge_features, hidden_edge_features, out_edge_features):
+        super().__init__(aggr='add')
+        self.mlp = nn.Sequential(
+            nn.Linear(2*n_node_features + n_edge_features, hidden_edge_features),
+            nn.ReLU(),
+            nn.Linear(hidden_edge_features, out_edge_features)
+        )
+
+    def forward(self, edge_index: Adj, x: Tensor, edge_attr: Tensor) -> Tensor:
+        edge_attr = self.edge_updater(edge_index, x=x, edge_attr=edge_attr)
+        return edge_attr
+
+    def edge_update(self, x_j: Tensor, x_i: Tensor, edge_attr: Tensor) -> Tensor:
+        out = torch.cat([x_i, x_j, edge_attr])
+        out = self.mlp(out)
+
+        return out
---
+++ mldyn/layers/layers.py
@@ -0,0 +1,52 @@
+import torch
+from torch_geometric.nn import MessagePassing
+
+
+class Node2Edge(MessagePassing):
+    def __init__(self, node_in_dim, edge_in_dim, edge_hidden_dim, edge_out_dim):
+        super().__init__(aggr="add")
+        self.node_in_dim = node_in_dim
+        self.edge_in_dim = edge_in_dim
+        self.edge_hidden_dim = edge_hidden_dim
+        self.edge_out_dim = edge_out_dim
+
+        self.mlp = torch.nn.Sequential(
+            torch.nn.Linear(2 * node_in_dim + edge_in_dim, edge_hidden_dim),
+            torch.nn.ReLU(),
+            torch.nn.Linear(edge_hidden_dim, edge_out_dim),
+        )
+
+    def forward(self, edge_index, x, edge_attr):
+        edge_attr = self.edge_updater(edge_index, x=x, edge_attr=edge_attr)
+        return edge_attr
+
+    def edge_update(self, x_j, x_i, edge_attr):
+        # pytorch geometric defines messages as going from node x_j to node x_i TODO: double check this definition...
+        out = torch.cat([x_j, x_i, edge_attr], dim=1)
+        out = self.mlp(out)
+        return out
+
+
+class Edge2Node(MessagePassing):
+    # TODO: need to eventually make a version of this that can handle multiple edge types? Have a different MLP for each edge type?
+    # (And make one edge type 0 explicitly)
+    def __init__(self, edge_in_dim, node_in_dim, node_hidden_dim, node_out_dim):
+        super().__init__(aggr="add")
+        self.edge_in_dim = edge_in_dim
+        self.node_in_dim = node_in_dim
+        self.node_hidden_dim = node_hidden_dim
+        self.node_out_dim = node_out_dim
+
+        self.mlp = torch.nn.Sequential(
+            torch.nn.Linear(2 * node_in_dim + edge_in_dim, node_hidden_dim),
+            torch.nn.ReLU(),
+            torch.nn.Linear(node_hidden_dim, node_out_dim),
+        )
+
+    def forward(self, edge_index, x, edge_attr):
+        return self.propogate(edge_index, x=x, edge_attr=edge_attr)
+
+    def message(self, x_j, x_i, edge_attr):
+        out = torch.cat([x_j, x_i, edge_attr], dim=1)
+        out = self.mlp(out)
+        return out
