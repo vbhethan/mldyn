dd1e5a190c6e011f54acd38cdfe8f3445d6107a5
Vignesh <vbhethan@caltech.edu>
2024-03-13 14:42:43 -0700

    updates layers to match paper implementation

--- mldyn/layers/layers.py
+++ mldyn/layers/layers.py
@@ -1,52 +1,117 @@
 import torch
-from torch_geometric.nn import MessagePassing
+import torch.nn as nn
+import torch.nn.functional as F
+
+from torch.autograd import Variable
+
+# See https://github.com/ethanfetaya/NRI/ and https://github.com/loeweX/AmortizedCausalDiscovery/
 
 
-class Node2Edge(MessagePassing):
-    def __init__(self, node_in_dim, edge_in_dim, edge_hidden_dim, edge_out_dim):
-        super().__init__(aggr="add")
-        self.node_in_dim = node_in_dim
-        self.edge_in_dim = edge_in_dim
-        self.edge_hidden_dim = edge_hidden_dim
-        self.edge_out_dim = edge_out_dim
+class MLP(nn.Module):
 
-        self.mlp = torch.nn.Sequential(
-            torch.nn.Linear(2 * node_in_dim + edge_in_dim, edge_hidden_dim),
-            torch.nn.ReLU(),
-            torch.nn.Linear(edge_hidden_dim, edge_out_dim),
+    def __init__(self, n_in, n_hid, n_out, do_prob=0.):
+        super(MLP, self).__init__()
+        self.fc1 = nn.Linear(n_in, n_hid)
+        self.fc2 = nn.Linear(n_hid, n_out) 
+        self.bn = nn.BatchNorm1d(n_out)
+        self.dropout_prob = do_prob
+
+        self.init_weights()
+
+        def init_weights(self):
+            for m in self.modules():
+                if isinstance(m, nn.Linear):
+                    nn.init.xavier_normal_(m.weight.data)
+                    m.bias.data.fill_(0.1)
+                elif isinstance(m, nn.BatchNorm1d):
+                    m.weight.data.fill_(1)
+                    m.bias.data.zero_()
+
+        def batch_norm(self, inputs):
+            x = inputs.view(inputs.size(0) * inputs.size(1), -1)
+            x = self.bn(x)
+            return x.view(inputs.size(0), inputs.size(1), -1)
+        
+        def forward(self, x):
+            x = F.elu(self.fc1(x))
+            x = F.droupout(x, self.dropout_prob, training=self.training)
+            x = F.elu(self.fc2(x)) 
+            return self.batch_norm(x)
+        
+class MLPEncoder(nn.Module):
+
+    def __init__(self, n_in, n_hid, n_out, do_prob=0.):
+        super(MLPEncoder, self).__init__()
+
+        self.mlp1 = MLP(n_in, n_hid, n_hid, do_prob)
+        self.mlp2 = MLP(n_hid * 2, n_hid, n_hid, do_prob)
+        self.mlp3 = MLP(n_hid, n_hid, n_hid, do_prob)
+        self.mlp4 = MLP(n_hid * 3, n_hid, n_hid, do_prob)
+
+        self.fc_out = nn.Linear(n_hid, n_out)
+        self.init_weights()
+
+    def init_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Linear):
+                nn.init.xavier_normal_(m.weight.data)
+                m.bias.data.fill_(0.1)
+            elif isinstance(m, nn.BatchNorm1d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+    
+    def edge2node(self, x, rel_rec, rel_send):
+        incoming = torch.matmul(rel_rec.t(), x)
+        return incoming / incoming.size(1)
+    
+    def node2edge(self, x, rel_rec, rel_send):
+        send = torch.matmul(rel_send, x)
+        rec = torch.matmul(rel_rec, x)
+        edges = torch.cat([send, rec], dim=2)
+        return edges
+    
+    def forward(self, inputs, rel_rec, rel_send):
+        # Inputs will have shape [num_sims, num_particles, num_timesteps, num_dims]
+        x = inputs.view(inputs.size(0), inputs.size(1), -1)
+        # New shape [num_sims, num_particles, num_timesteps * num_dims]
+        x = self.mlp1(x)
+        x = self.node2edge(x, rel_rec, rel_send)
+        x = self.mlp2(x)
+        x_skip = x
+
+        # Implement the factor graph to start 
+        x = self.edge2node(x, rel_rec, rel_send)
+        x = self.mlp3(x)
+        x = self.node2edge(x, rel_rec, rel_send)
+        x = torch.cat([x, x_skip], dim=2)
+        x = self.mlp4(x)
+
+        return self.fc_out(x)
+
+
+class MLPDecoder(nn.Module):
+    def __init__(self, n_in_node, edge_types, msg_hid, msg_out, n_hid, do_prob=0., skip_first=False):
+        super(MLPDecoder, self).__init__()
+        self.msg_fc1 = nn.ModuleList(
+            [nn.Linear(2 * n_in_node, msg_hid) for _ in range(edge_types)]
         )
-
-    def forward(self, edge_index, x, edge_attr):
-        edge_attr = self.edge_updater(edge_index, x=x, edge_attr=edge_attr)
-        return edge_attr
-
-    def edge_update(self, x_j, x_i, edge_attr):
-        # pytorch geometric defines messages as going from node x_j to node x_i TODO: double check this definition...
-        out = torch.cat([x_j, x_i, edge_attr], dim=1)
-        out = self.mlp(out)
-        return out
-
-
-class Edge2Node(MessagePassing):
-    # TODO: need to eventually make a version of this that can handle multiple edge types? Have a different MLP for each edge type?
-    # (And make one edge type 0 explicitly)
-    def __init__(self, edge_in_dim, node_in_dim, node_hidden_dim, node_out_dim):
-        super().__init__(aggr="add")
-        self.edge_in_dim = edge_in_dim
-        self.node_in_dim = node_in_dim
-        self.node_hidden_dim = node_hidden_dim
-        self.node_out_dim = node_out_dim
-
-        self.mlp = torch.nn.Sequential(
-            torch.nn.Linear(2 * node_in_dim + edge_in_dim, node_hidden_dim),
-            torch.nn.ReLU(),
-            torch.nn.Linear(node_hidden_dim, node_out_dim),
+        self.msg_fc2 = nn.ModuleList(
+            [nn.Linear(msg_hid, msg_out) for _ in range(edge_types)]
         )
+        self.msg_out_shape = msg_out
+        self.skip_first_edge_type = skip_first
 
-    def forward(self, edge_index, x, edge_attr):
-        return self.propagate(edge_index, x=x, edge_attr=edge_attr)
+        self.out_fc1 = nn.Linear(n_in_node + msg_out, n_hid)
+        self.out_fc2 = nn.Linear(n_hid, n_hid)
+        self.out_fc3 = nn.Linear(n_hid, n_in_node)
 
-    def message(self, x_j, x_i, edge_attr):
-        out = torch.cat([x_j, x_i, edge_attr], dim=1)
-        out = self.mlp(out)
-        return out
+        self.dropout_prob = do_prob
+
+    def single_step_forward(self, single_timestep_inputs, rel_rec, rel_send, single_timestep_rel_type):
+        # single_timesteps_inputs has shape [batch_size, num_timesteps, num_particles, num_dims]
+        # single_timestep_rel_type has shape [batch_size, num_timesteps, num_particles*(num_particles-1), num_edge_types]
+
+        receivers = torch.matmul(rel_rec, single_timestep_inputs)
+        senders = torch.matmul(rel_send, single_timestep_inputs)
+        pre_msg = torch.cat([receivers, senders], dim=-1)
+        
